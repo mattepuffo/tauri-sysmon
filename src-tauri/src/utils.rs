1c85d2dbde1fc93b153b4498d7c5706f1fae18c7
mattepuffo <matteo.ferrone@hotmail.it>
2026-07-03 14:25:14 +0200

    windows

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,3 +1,12 @@
+use windows::Networking::Connectivity::NetworkInformation;
+
+/// Funzione per visualizzare il nome  dell'SSID attivo
+/// Per Windows è richiesto un crate apposito: https://github.com/microsoft/windows-rs
+///
+/// Dipendenza da aggiungere nel caso di programma multi piattaforma:
+///
+/// [target.'cfg(target_os = "windows")'.dependencies]
+/// windows = { version = "0.62.2", features = ["Devices_WiFi", "Networking_Connectivity"] }
 pub fn get_wifi_ssid() -> Option<String> {
     #[cfg(target_os = "linux")]
     {
@@ -44,17 +53,13 @@ pub fn get_wifi_ssid() -> Option<String> {
 
     #[cfg(target_os = "windows")]
     {
-        let output = std::process::Command::new("netsh")
-            .args(["wlan", "show", "interfaces"])
-            .output()
+        let profile = NetworkInformation::GetInternetConnectionProfile().ok()?;
+        let ssid = profile
+            .WlanConnectionProfileDetails()
+            .ok()?
+            .GetConnectedSsid()
             .ok()?;
 
-        let stdout = String::from_utf8(output.stdout).ok()?;
-
-        stdout
-            .lines()
-            .find(|l| l.trim_start().starts_with("SSID") && !l.contains("BSSID"))
-            .and_then(|l| l.split(':').nth(1))
-            .map(|s| s.trim().to_string())
+        Some(ssid.to_string())
     }
 }
